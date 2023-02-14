af3909c206b92e3b80a6dd39946c06bc67ca7bf7
Docs Robot <robot@example.com>
2023-02-14 15:38:24 +0000

    Committing documentation changes

--- docs/navtreedata.js
+++ docs/navtreedata.js
@@ -25,9 +25,7 @@
 var NAVTREE =
 [
   [ "Soldered MOSFET with easyC Arduino library", "index.html", [
-    [ "MOSFET with easyC Arduino library", "index.html", [
-      [ "Have fun!", "index.html#autotoc_md7", null ]
-    ] ],
+    [ "MOSFET with easyC Arduino library", "index.html", "index" ],
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
